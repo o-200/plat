6af3ea1aa3804f12ed70e5ce283fcf71cb6d3ed8
Alex Abramov <omgout.200@gmail.com>
2025-10-25 16:13:06 +0700

    describe swagger blocks

--- src/blocks/blocks.controller.ts
+++ src/blocks/blocks.controller.ts
@@ -10,27 +10,61 @@ import {
 import { BlocksService } from './blocks.service';
 import { CreateBlockDto } from './dto/create-block.dto';
 import { UpdateBlockDto } from './dto/update-block.dto';
+import {
+  ApiBody,
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger';
 
 @Controller('blocks')
 export class BlocksController {
   constructor(private readonly blocksService: BlocksService) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'Создание блока.',
+    description: `Создание блока, который содержит внутри себя другие элементы по типу kind.
+      тип CONTENT используется для хранения постов;
+      тип SERVICE используется для хранения тегов услуг;
+    `,
+  })
+  @ApiBody({ type: CreateBlockDto })
+  @ApiCreatedResponse({
+    type: CreateBlockDto,
+    isArray: true,
+  })
   create(@Body() createBlockDto: CreateBlockDto) {
     return this.blocksService.create(createBlockDto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Список блоков',
+  })
+  @ApiBody({ type: CreateBlockDto })
+  @ApiResponse({
+    type: CreateBlockDto,
+    isArray: true,
+  })
   findAll() {
     return this.blocksService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Поиск блока по ID',
+  })
+  @ApiBody({ type: CreateBlockDto })
+  @ApiResponse({ type: CreateBlockDto })
   findOne(@Param('id') id: string) {
     return this.blocksService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Обновление блока' })
+  @ApiBody({ type: UpdateBlockDto })
+  @ApiResponse({ type: UpdateBlockDto })
   update(@Param('id') id: string, @Body() updateBlockDto: UpdateBlockDto) {
     return this.blocksService.update(+id, updateBlockDto);
   }
--- src/blocks/dto/create-block.dto.ts
+++ src/blocks/dto/create-block.dto.ts
@@ -1,11 +1,16 @@
 import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { $Enums } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateBlockDto {
+  @ApiProperty({ description: 'Название блока' })
   @IsString()
   @IsNotEmpty()
   title!: string;
 
+  @ApiProperty({
+    description: `Тип блока. "CONTENT" - блок с информацией/посты "SERVICE" - блок с услугами`,
+  })
   @IsOptional()
   @IsEnum($Enums.Kind)
   kind?: $Enums.Kind;
