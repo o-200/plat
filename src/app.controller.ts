f3c74e7ac97675bb732f8e1140c82870c1188b27
Alex Abramov <omgout.200@gmail.com>
2025-10-25 16:20:07 +0700

    add /health

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('health')
+  @ApiOperation({
+    summary: 'Проверки работоспособности сервиса.',
+    description:
+      `Тестовый endpoint для проверки работы сервиса. Всегда возвращает 200 ОК и тело "Hello, World!"`,
+    })
+  @ApiResponse({
+    type: "Hello, World!",
+  })
   getHello(): string {
     return this.appService.getHello();
   }
