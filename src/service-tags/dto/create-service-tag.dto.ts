b17fc8f1572420ad10ddee132be95c64cf5ffccf
o-200 <omgout.200@gmail.com>
2025-11-01 15:07:31 +0700

    add priority to ServiceTags

--- src/service-tags/dto/create-service-tag.dto.ts
+++ src/service-tags/dto/create-service-tag.dto.ts
@@ -1,9 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsInt, IsNotEmpty, IsOptional, IsString, IsUrl } from 'class-validator';
+import {
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  IsUrl,
+} from 'class-validator';
 
 export class CreateServiceTagDto {
-  @ApiProperty({ description: 'Название тега услуги. Пример - Дом и ЖКУ, Связь и телефоны...', })
+  @ApiProperty({
+    description:
+      'Название тега услуги. Пример - Дом и ЖКУ, Связь и телефоны...',
+  })
   @IsString()
   @IsNotEmpty()
   title!: string;
@@ -15,6 +26,18 @@ export class CreateServiceTagDto {
   @IsOptional()
   @IsNotEmpty()
   imageLink: string;
+
+  @ApiProperty({
+    description: `Номер приоритета. Необходимо для показа блоков (GET запросы) исходя из этого поля. Чем меньше значение - тем больше приоритет.
+    Если не заполнено - присуждается автоматически низший приоритет.
+    `,
+  })
+  @IsOptional()
+  @IsNumber()
+  @IsInt()
+  @IsPositive()
+  @Type(() => Number)
+  priority?: number;
 
   @ApiProperty({
     description: 'Идентификатор Блока',
